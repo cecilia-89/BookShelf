5e56d26c3e4c1b6c1e8a4c49a47213f360395f88
Cecilia Atabong <catabong89@gmail.com>
2022-04-29 05:45:39 +0100

    edited: function view(book)

--- shelf/views.py
+++ shelf/views.py
@@ -5,13 +5,14 @@ from shelf.models import *
 
 def index(request):
 
-    return render(request, 'index.html', {'books':books})
+    return render(request, 'index.html')
 
 
 def books(request):
-    
-    return render(request, 'booklist.html',{'lists':lists})
+
+
+    return render(request, 'booklist.html')
 
 def book(request, book_id):
-    
-    return render(request, 'book.html',{'book':book})
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'book.html', {'book': book})
